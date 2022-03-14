f29f96826fb149493e56295fffbe69f8cf07630d
Fortan Pireva <fortanpireva99@gmail.com>
2022-03-14 22:53:57 +0100

    complete basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import ToDo from "./components/Todo";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My todos</h1>
+      <ToDo text="Hello my friend" />
+      <ToDo text="Hello my I love you " />
+      <ToDo text="Hello my Oh my " />
     </div>
   );
 }
